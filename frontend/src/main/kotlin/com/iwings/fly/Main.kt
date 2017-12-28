5486aa48a2533c8de86bf3422b0a13e1e619d6cb
artemijbezguzikov <artemii.bezguzikov@gmail.com>
2017-12-28 19:48:40 +0300

    fix scripts

--- frontend/src/main/kotlin/com/iwings/fly/Main.kt
+++ frontend/src/main/kotlin/com/iwings/fly/Main.kt
@@ -3,7 +3,4 @@ package com.iwings.fly
 fun main(args: Array<String>) {
     val post = TwitterPostDto("Heheheh", 100500)
     println(post)
-    println("Hahah its a win motherfucker")
-    println("I will changed!")
-    println("It is working now")
 }
